b2f9c4c188a4bc569f25511d893db26bf6b16134
Mladen Reljic <mladen.reljic87@gmail.com>
2026-05-13 14:34:11 +0200

    fix: ensure worklet.js files are included in assets

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
   plugins: [svelte()],
+  assetsInclude: ['**/*.worklet.js'],
   resolve: {
     alias: {
       '$lib': resolve(__dirname, 'src/lib'),
